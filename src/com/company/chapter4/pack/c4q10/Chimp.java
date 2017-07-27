c5b9d7cdf51885ff984241e49be9e4c70ea1a7f3
Aleksey Danilov <alex_danilov93@mail.ru>
2017-07-27 09:30:44 +0300

    Добавил 10 задачу по четвертой главе

---
+++ src/com/company/chapter4/pack/c4q10/Chimp.java
@@ -0,0 +1,10 @@
+import com.company.chapter4.pack.c4q10.rope.*;
+import static com.company.chapter4.pack.c4q10.rope.Rope.*;
+
+public class Chimp {
+    public static void main(String[] args) {
+        Rope.swing();
+        new Rope().swing();
+        System.out.println(LENGTH);
+    }
+}
---
+++ src/com/company/chapter4/pack/c4q10/rope/Rope.java
@@ -0,0 +1,14 @@
+package com.company.chapter4.pack.c4q10.rope;
+
+
+public class Rope {
+    public static int LENGTH = 5;
+
+    static {
+        LENGTH = 10;
+    }
+
+    public static void swing() {
+        System.out.print("swing ");
+    }
+}
