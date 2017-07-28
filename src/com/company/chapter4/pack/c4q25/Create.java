431d2e80b2b1e85d0ddde83caac8e382ebe68d14
Aleksey Danilov <alex_danilov93@mail.ru>
2017-07-29 00:22:09 +0300

    Добавил 25 задачу из четвертой главы

---
+++ src/com/company/chapter4/pack/c4q25/Create.java
@@ -0,0 +1,28 @@
+package com.company.chapter4.pack.c4q25;
+
+public class Create {
+    Create() {
+        System.out.print("1 ");
+    }
+
+   /* Create(int num) {
+        System.out.print("2 ");
+    }*/
+
+    Create(Integer num) {
+        System.out.print("3 ");
+    }
+
+    Create(Object num) {
+        System.out.print("4 ");
+    }
+
+    Create(int... nums) {
+        System.out.print("5 ");
+    }
+
+    public static void main(String[] args) {
+        new Create(100);
+        new Create(1000L);
+    }
+}
