34644cabf59c258ca37c597c7a2a7da08aaad7f9
Aleksey Danilov <alex_danilov93@mail.ru>
2017-07-31 22:27:13 +0300

    ДОбавил 2 задачу из пятой главы с описанием.

---
+++ src/com/company/chapter5/pack/c5q2/Platypus.java
@@ -0,0 +1,13 @@
+package com.company.chapter5.pack.c5q2;
+
+public class Platypus extends Mammal {
+    public Platypus() { //будет ошибка компиляции, так как класс от которого мы наследуемся не имеет конструктора по
+        // дефолту. Но конструктор класса наследника первым делом вызывает super() без аргкментов. Чтобы ошибки не
+        // было следует добавить в родительский класс, конструктор без аргументов.
+        System.out.print("Platypus");
+    }
+
+    public static void main(String[] args) {
+        new Mammal(5);
+    }
+}
