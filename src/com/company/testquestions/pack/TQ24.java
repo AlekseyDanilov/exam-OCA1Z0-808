0042aea4fed0148fc11d80b5b9d7d946faa86462
Aleksey Danilov <alex_danilov93@mail.ru>
2017-09-06 07:56:42 +0300

    Добавил 24 задачу из теста с объяснением, почему так.

---
+++ src/com/company/testquestions/pack/TQ24.java
@@ -0,0 +1,12 @@
+package com.company.testquestions.pack;
+
+
+public class TQ24 {
+    public static void question24() {
+//        Ошибка компиляции, так как в цикле for, последний элемент не должен возвращать boolean значение.
+//        for (; ; Math.random() < 0.5) {     System.out.println("true");}
+
+//        Это приведет к бесконечному циклу, но не вызовет ошибки компиляции.
+//        for (; ; Math.random()) {     System.out.println("true");}
+    }
+}
